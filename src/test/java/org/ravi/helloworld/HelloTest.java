1213bbaf4bde9c06e2398b0c56e4c7d3a9407793
Training_C2a.05.06 <Training_C2a.05.06@accenture.com>
2018-11-15 12:04:41 +0530

    fourth commit

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -19,7 +19,7 @@ public class HelloTest {
 	@BeforeTest
 	public void beforeTest() {
 		
-		System.setProperty("webdriver.firefox.marionette","resources\\gecko.exe");
+		System.setProperty("webdriver.firefox.marionette","resources\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.get("http://demoaut.com");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
